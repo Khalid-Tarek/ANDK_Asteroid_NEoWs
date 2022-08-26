8ba09776fb47d9987df8ee6ff80101c2b6647c87
Khalid Tarek <khalid.t.mostafa@gmail.com>
2022-08-26 21:59:00 +0200

    Created entity and DAO

--- starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -1,7 +1,16 @@
 package com.udacity.asteroidradar
 
+import android.net.Uri
+
 object Constants {
-    const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
+    const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
+    const val API_KEY = "qdhd0YuuWw22qi37uNoDQbYyFIcYai3D9Fuuoz3a"
+    val MY_URL = Uri.parse(BASE_URL)
+        .buildUpon()
+        .appendPath("planetary")
+        .appendPath("apod")
+        .appendQueryParameter("api_key", API_KEY)
+        .build()
 }
---
+++ starter/app/src/main/java/com/udacity/asteroidradar/database/AsteroidDao.kt
@@ -0,0 +1,29 @@
+package com.udacity.asteroidradar.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface AsteroidDao {
+    @Insert
+    suspend fun insert(asteroid: Asteroid)
+
+    @Update
+    suspend fun update(asteroid: Asteroid)
+
+    @Delete
+    suspend fun delete(asteroid: Asteroid)
+
+    @Query("SELECT * FROM asteroid_table WHERE id = :value")
+    suspend fun get(value: Long): Asteroid?
+
+    @Query("DELETE FROM asteroid_table")
+    suspend fun clear()
+
+    @Query("SELECT * FROM asteroid_table ORDER BY id DESC")
+    fun getAllAsteroids(): LiveData<List<Asteroid>>
+
+    //TODO: Delete all asteroids before today
+    //@Query("DELETE FROM asteroid_table WHERE ")
+    //suspend fun deleteOldAsteroids()
+}
