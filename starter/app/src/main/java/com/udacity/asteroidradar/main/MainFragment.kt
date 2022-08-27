ca7ad4769a27545e4faeafe0215addc073ee189b
Khalid Tarek <khalid.t.mostafa@gmail.com>
2022-08-27 20:28:17 +0200

    Continued Implementing ViewModel

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -3,11 +3,15 @@ package com.udacity.asteroidradar.main
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.database.AsteroidDatabase
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
+import com.udacity.asteroidradar.detail.DetailFragment
+import com.udacity.asteroidradar.detail.DetailFragmentArgs
 
 class MainFragment : Fragment() {
 
@@ -15,20 +19,25 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
         val binding = FragmentMainBinding.inflate(inflater)
 
-        setHasOptionsMenu(true)
-
         val application = requireNotNull(this.activity).application
-
         val dataSource = AsteroidDatabase.getInstance(application).asteriodDao
-
         val mainViewModelFactory = MainViewModelFactory(dataSource, application)
-
         viewModel = ViewModelProvider(this, mainViewModelFactory).get(MainViewModel::class.java)
 
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+
+        viewModel.navigateToAsteroid.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                findNavController().navigate(MainFragmentDirections.actionShowDetail(it))
+                viewModel.doneNavigating()
+            }
+        })
+
+
 
         return binding.root
     }
